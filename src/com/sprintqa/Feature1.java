101ae5a8a4728b8eb209aa641f2bd803676b5228
NodiraMama <mail.nodira1@gmail.com>
2020-04-30 19:01:44 -0400

    Updated feature1

--- src/com/sprintqa/Feature1.java
+++ src/com/sprintqa/Feature1.java
@@ -3,5 +3,6 @@ package com.sprintqa;
 public class Feature1 {
 	public static void main(String[] args) {
 		System.out.println("Feature1 task completed");
+		System.out.println ("Additional changes added");
 	}
 }
